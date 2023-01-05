e9f27d2fc2687836877025eb555b21d7bf10e516
vtrtga <vitor.ypvalim@gmail.com>
2023-01-05 17:22:13 -0400

    customer router get method/getAllCustomers

--- app/backend/src/routers/CustomerRouter.ts
+++ app/backend/src/routers/CustomerRouter.ts
@@ -1,8 +1,9 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import CustomerController from "../Controllers/CustomerController";
 
 const CustomerRouter = Router();
 
 CustomerRouter.post('/', (req, res, next) => new CustomerController(req, res, next).createCustomer());
+CustomerRouter.get('/', (req, res, next) => new CustomerController(req, res, next).getAllCustomers());
 
 export default CustomerRouter;
