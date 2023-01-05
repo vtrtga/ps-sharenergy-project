56f18746c00e39593743cdfcec3e3841ffac16f5
vtrtga <vitor.ypvalim@gmail.com>
2023-01-05 11:20:58 -0400

    fixing database container error

--- app/backend/db/seed.js
+++ app/backend/db/seed.js
@@ -4,11 +4,33 @@ const customerCollection = 'customer';
 
 use(database);
 
-db.createCollection(userCollection);
-db.createCollection(customerCollection);
-
 db.user.drop();
+
+db.createCollection(userCollection);
+
+
+db.user.insertMany([
+  {
+    _id: "63b6dca59536b3acfca82fa0",
+    name: "Joe Smith",
+    username: "desafiosharenergy",
+    password: "sh@r3n3rgy",
+    birthDate: "10/02/1994",
+    gender: "female"
+  },
+  {
+    _id: "63b6dcb559c07d581bbe043e",
+    name: "Johann Smith",
+    username: "desafiosharenergy2",
+    password: "sh@r3n3rgy2",
+    birthDate: "10/02/1994",
+    gender: "male"
+  }
+]);
+
 db.customer.drop();
+
+db.createCollection(customerCollection);
 
 db.customer.insertMany([
   {
@@ -40,32 +62,4 @@ db.customer.insertMany([
   }
 ])
 
-/*
- id?: string | undefined;
-  birthDate: string;
-  name: string;
-  gender?: string;
-  phone: string;
-  email: string;
-  cpf: string;
-  address: string;
-*/
 
-db.user.insertMany([
-  {
-    _id: "63b6dca59536b3acfca82fa0",
-    name: "Joe Smith",
-    username: "desafiosharenergy",
-    password: "sh@r3n3rgy",
-    birthDate: "10/02/1994",
-    gender: "female"
-  },
-  {
-    _id: "63b6dcb559c07d581bbe043e",
-    name: "Johann Smith",
-    username: "desafiosharenergy2",
-    password: "sh@r3n3rgy2",
-    birthDate: "10/02/1994",
-    gender: "male"
-  }
-]);
