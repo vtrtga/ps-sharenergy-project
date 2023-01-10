e1f41ec6e735d2b647ad9737e2154593efe64862
vtrtga <vitor.ypvalim@gmail.com>
2023-01-10 15:35:03 -0400

    customers gender select input styles

--- app/frontend/src/components/GenericSelectInput.jsx
+++ app/frontend/src/components/GenericSelectInput.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 
 function GenericSelectInput({ options, onChange, name }) {
   return (
-    <div>
-      <select onChange={ onChange }>
+    <div className="block">
+      <select
+        className="md: form-control
+       block border-solid border-2
+       rounded-md mx-auto p-2 outline-black"
+        onChange={ onChange }
+      >
         {
           options.map((o, i) => (
-            <option key={ i } name={ name } value={ o.value }>{ o.value }</option>
+            <option key={ i } name={ name } value={ o }>{ o }</option>
           ))
         }
       </select>
--- app/frontend/src/pages/Customers.jsx
+++ app/frontend/src/pages/Customers.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable max-len */
 import React, { useState } from 'react';
+import Button from '../components/Button';
 import GenericSelectInput from '../components/GenericSelectInput';
 import Input from '../components/Input';
 import Nav from '../components/Nav';
@@ -24,22 +26,25 @@ function Customers() {
     console.log(newCustomer);
   };
 
-  const opt = ['male', 'female', 'non binary'];
+  const opt = ['Select gender', 'male', 'female', 'non binary'];
   return (
     <div>
       <Nav />
-      <section>
-        <Input placeHolder="Name" name="name" onChange={ handleOnNewCustomer } />
-        <Input placeHolder="Email" name="email" onChange={ handleOnNewCustomer } />
-        <Input placeHolder="Phone" name="phone" onChange={ handleOnNewCustomer } />
-        <Input placeHolder="CPF" name="cpf" onChange={ handleOnNewCustomer } />
-        <Input placeHolder="Address" name="address" onChange={ handleOnNewCustomer } />
-        <GenericSelectInput
-          options={ opt }
-          name="gender"
-          onChange={ handleOnNewCustomer }
-        />
-      </section>
+      <form className="flex md: m-auto">
+        <section className="md: mx-auto mt-8 shadow-md rounded-md p-5">
+          <Input placeHolder="Name" name="name" onChange={ handleOnNewCustomer } />
+          <Input placeHolder="Email" name="email" onChange={ handleOnNewCustomer } />
+          <Input placeHolder="Phone" name="phone" onChange={ handleOnNewCustomer } />
+          <Input placeHolder="CPF" name="cpf" onChange={ handleOnNewCustomer } />
+          <Input placeHolder="Address" name="address" onChange={ handleOnNewCustomer } />
+          <GenericSelectInput
+            options={ opt }
+            name="gender"
+            onChange={ handleOnNewCustomer }
+          />
+          <Button text="Register" />
+        </section>
+      </form>
     </div>
   );
 }
